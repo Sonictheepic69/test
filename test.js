2033a56ee22ec79c406430dd4bad4ba7d779acba
Sonictheepic69 <70746937+Sonictheepic69@users.noreply.github.com>
2023-11-01 17:45:50 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-console.log("hi")
+var map = {
+    title: "Orbit",
+    song: "env2",
+    maker: "applepear",
+    spawn: [0, 0.5, 0],
+    init: function () {
+      a.p([0, 0, 0], [0, 0, 0], [5, 2, 100], "ffffff", 0, 0, 0.6, true, false, false, false);
+    }
+}
