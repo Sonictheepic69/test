f89d9b4bc2af82a6daef10262415a24018e4b85a
Sonictheepic69 <70746937+Sonictheepic69@users.noreply.github.com>
2023-11-01 18:00:03 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 var map = {
-    title: "Orbit",
+    title: "Test",
     song: "env2",
-    maker: "applepear",
+    maker: "Sonictheepic69",
     spawn: [0, 0.5, 0],
     init: function () {
       a.p([0, 0, 0], [0, 0, 0], [5, 2, 100], "ffffff", 0, 0, 0.6, true, false, false, false);
-    }
+    },
+    physics_update: function() {},
+    render_update: function() {}
 }
