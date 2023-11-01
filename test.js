42b462198bd70e9200d42e01854d80029c2cf83a
Sonictheepic69 <70746937+Sonictheepic69@users.noreply.github.com>
2023-11-01 19:10:52 +0800

    Update test.js

--- test.js
+++ test.js
@@ -7,22 +7,21 @@ var map = {
       a.p([0, 0, 0], [0, 0, 0], [5, 2, 100], "ffffff", 0, 0, 0.6, true, false, false, false);
     },
     post: function() {
-        a.m('P0').unfreezeWorldMatrix();
-        a.m('P0').material = decorations.materials.moving_mat;
+        a.u('P0')
     },
     section_id: 0,
     reset: function() {
         this.section_id = 0;
-        a.m('C0').rotationQuaternion = BABYLON.Quaternion.RotationAxis(new BABYLON.Vector3(0,0,0), 0);
-        a.m('C0').rotation.x = 0;
-        a.m('C0').rotation.y = 0;
-        a.m('C0').rotation.z = 0;
-        a.m('C0').position.x = 0;
-        a.m('C0').position.y = 0;
-        a.m('C0').position.z = 0;
-        a.m('C0').scaling.x = 2;
-        a.m('C0').scaling.y = 2;
-        a.m('C0').scaling.z = 2;
+        a.m('P0').rotationQuaternion = BABYLON.Quaternion.RotationAxis(new BABYLON.Vector3(0,0,0), 0);
+        a.m('P0').rotation.x = 0;
+        a.m('P0').rotation.y = 0;
+        a.m('P0').rotation.z = 0;
+        a.m('P0').position.x = 0;
+        a.m('P0').position.y = 0;
+        a.m('P0').position.z = 0;
+        a.m('P0').scaling.x = 2;
+        a.m('P0').scaling.y = 2;
+        a.m('P0').scaling.z = 2;
     },
     physics_update: function() {},
     render_update: function() {}
