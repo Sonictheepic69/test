36e93ca021e235e460726f9e431c890fe951fd90
Sonictheepic69 <70746937+Sonictheepic69@users.noreply.github.com>
2023-11-11 08:14:39 +0800

    Update around_the_world.js

--- around_the_world.js
+++ around_the_world.js
@@ -1,7 +1,7 @@
 var map = {
     title: "Around the World",
     song: "env2",
-    maker: "SourLemons, Bumpo, Zoni, Zhou Yu, Sonictheepic69, Maple and Co. Mapmaking, Hjonk, ItsDukki, ChappJack, Wienerdog, Painkiller, and Sully",
+    maker: "SourLemons, Bumpo, Zoni, Zhou Yu, Sonictheepic69, Maple and Co. Mapmaking, Hjonk, ItsDukki, ChappJack, Wienerdog, Painkiller, Sully, and applepear",
     spawn: [0, 0.5, 0],
     init: function() {
         a.p([-10.42, -33.38, -412.93], [0, 0, 0], [2000, 2, 195.7], "0054c9", 0, 0, 0.6, true, false, true, false);
